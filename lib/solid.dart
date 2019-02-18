a9bab170079177133c3337b1c806d318b4d1ecbc
James Madden <jamesmadden@James-MacBook-Air.local>
2019-02-17 23:14:49 -0800

    Add Some Documentation

--- lib/solid.dart
+++ lib/solid.dart
@@ -4,17 +4,24 @@ library solid;
 import 'package:js/js.dart';
 
 @JS('auth')
+/// Wrapper for solid.auth
 class Auth {
   external static fetch(info, Object options);
+  /// Login a User
   external static login(String idp, LoginOptions options);
+  /// Open a Popup for Login
   external static popupLogin(LoginOptions options);
+  /// Get the current user
   external static currentSession([storage]);
+  /// Run [callback] when the user signs in or out
   external static trackSession(Function callback);
+  /// Logout the current user
   external static logout([storage]);
 }
 
 @JS()
 @anonymous
+/// Options for the login methods on the Auth class.
 class LoginOptions {
   external String get callbackUri;
   external set callbackUri(String v);
